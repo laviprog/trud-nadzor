b5fcf6b4b90ac22c5695f708e1b4df584a36270b
laviprog <areyouready@internet.ru>
2025-04-27 22:33:05 +0300

    update footer ui

--- src/components/footer/SocialLinks.tsx
+++ src/components/footer/SocialLinks.tsx
@@ -10,23 +10,23 @@ const socials: SocialLink[] = [
   {
     name: 'Telegram',
     href: '#',
-    icon: <Image src="/tg.png" alt="Telegram" width={35} height={35} />,
+    icon: <Image src="/socials/tg.png" alt="Telegram" width={35} height={35} />,
   },
   {
     name: 'VKontakte',
     href: '#',
-    icon: <Image src="/vk.png" alt="Vkontakte" width={35} height={35} />,
+    icon: <Image src="/socials/vk.png" alt="Vkontakte" width={35} height={35} />,
   },
   {
     name: 'Whatsapp',
     href: '#',
-    icon: <Image src="/whatsapp.png" alt="WhatsApp" width={35} height={35} />,
+    icon: <Image src="/socials/whatsapp.png" alt="WhatsApp" width={35} height={35} />,
   },
 ];
 
 export default function SocialLinks() {
   return (
-    <ul className="flex justify-center mt-6 gap-11 max-lg:pr-5 lg:justify-start">
+    <ul className="flex justify-center mt-6 gap-11 lg:justify-start">
       {socials.map((social) => (
         <li key={social.name}>
           <a href={social.href} target="_blank" rel="noopener noreferrer">
